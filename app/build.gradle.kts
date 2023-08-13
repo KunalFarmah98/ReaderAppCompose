cd718da982f2f31753bbf673a770c465462101c5
Kunal Farmah <kunalfarmah98@gmail.com>
2023-08-13 16:34:16 +0530

    inital commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,100 @@
+import Versions
+
+plugins {
+    kotlin("kapt")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.kunalfarmah.apps.readerapp"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.kunalfarmah.apps.readerapp"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.0")
+
+    implementation(platform("androidx.compose:compose-bom:${Versions.compose_bom_version}"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:${Versions.firebase_bom_version}"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:${Versions.hilt_version}")
+    kapt("com.google.dagger:hilt-android-compiler:${Versions.hilt_version}")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutine_version}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutine_version}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:${Versions.coroutine_version}")
+
+    // Coroutine Lifecycle Scopes
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle_version}")
+
+    //lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycle_version}")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:1.4.0")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+    const val compose_version = "1.0.5"
+    const val hilt_version = "2.44"
+    const val room_version = "2.5.1"
+    const val firebase_bom_version = "32.2.2"
+    const val compose_bom_version = "2023.03.00"
+    const val coroutine_version = "1.7.3"
+    const val lifecycle_version = "2.6.1"
+}
